355ef2cb0cb33371376ea939e7023decdeea9e79
RaulLeon01 <20243rd013@utez.edu.mx>
2025-07-15 11:59:22 -0600

    Listo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.peliculascolab {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.peliculascolab to javafx.fxml;
+    exports org.example.peliculascolab;
+}
